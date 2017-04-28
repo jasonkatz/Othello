4197f7e056122efdc160366a2759e404a4ba0924
Jason Katz <jkatz94@gmail.com>
2017-04-28 15:22:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cube
+Rubik's Cube simulator for ECE462 at The Cooper Union
---
+++ www/js/main.js
@@ -0,0 +1,12 @@
+/*
+Jason Katz
+ECE-462 Project 2: Othello
+
+File: main.js
+Description: WebGL display functionality and interface logic
+*/
+
+"use strict";
+
+var gl;
+var canvas;
